955201f2e0da583f9a82a59b7f256605668ee31b
Jarno Elovirta <jarno.elovirta@wunderdog.fi>
2019-08-02 12:51:58 +0300

    Fix logging and conversion error handling

--- docker/main.go
+++ docker/main.go
@@ -8,49 +8,41 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
-
-	//"os/exec"
 	"path/filepath"
 )
 
 type Args struct {
-	src *url.URL;
-	dst *url.URL;
-	tmp string;
-	out string;
+	src *url.URL
+	dst *url.URL
+	tmp string
+	out string
 }
 
 func readArgs() *Args {
 	input := os.Getenv("input")
 	if input == "" {
 		log.Fatalf("Input environment variable not set")
-		os.Exit(1)
 	}
 	output := os.Getenv("output")
 	if output == "" {
 		log.Fatalf("Output environment variable not set")
-		os.Exit(1)
 	}
 	src, err := url.Parse(input)
 	if err != nil {
-		log.Fatalf("Failed to parse input argument %s: %s", input, err.Error())
-		os.Exit(1)
+		log.Fatalf("Failed to parse input argument %s: %v", input, err)
 	}
 	dst, err := url.Parse(output)
 	if err != nil {
-		log.Fatalf("Failed to parse output argument %s: %s", output, err.Error())
-		os.Exit(1)
+		log.Fatalf("Failed to parse output argument %s: %v", output, err)
 	}
 
 	tmp, err := ioutil.TempDir("", "tmp")
 	if err != nil {
-		log.Fatalf("Failed to create temporary directory: %s", err.Error())
-		os.Exit(1)
+		log.Fatalf("Failed to create temporary directory: %v", err)
 	}
 	out, err := ioutil.TempDir("", "out")
 	if err != nil {
-		log.Fatalf("Failed to create temporary directory: %s", err.Error())
-		os.Exit(1)
+		log.Fatalf("Failed to create temporary directory: %v", err)
 	}
 	return &Args{src, dst, tmp, out}
 }
@@ -72,13 +64,9 @@ func convert(dir string) error {
 			cmd := exec.Command("convert", src, dst)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
-			//cmd.Env = append(os.Environ(),
-			//	"FOO=duplicate_value", // ignored
-			//	"FOO=actual_value",    // this value is used
-			//)
+
 			if err := cmd.Run(); err != nil {
-				fmt.Printf("ERROR: Failed to convert: %s\n", err.Error())
-				return nil
+				return fmt.Errorf("ERROR: Failed to convert: %v", err)
 			}
 		}
 		return nil
@@ -90,17 +78,14 @@ func main() {
 	args := readArgs()
 
 	if _, err := kuhnuri.DownloadFile(args.src, args.tmp); err != nil {
-		log.Fatalf("Failed to download %s: %s", args.src, err.Error())
-		os.Exit(1)
+		log.Fatalf("Failed to download %s: %v", args.src, err)
 	}
 
 	if err := convert(args.tmp); err != nil {
-		log.Fatalf("Failed to convert %s: %s", args.tmp, err.Error())
-		os.Exit(1)
+		log.Fatalf("Failed to convert %s: %v", args.tmp, err)
 	}
 
 	if err := kuhnuri.UploadFile(args.tmp, args.dst); err != nil {
-		log.Fatalf("Failed to upload %s: %s", args.dst, err.Error())
-		os.Exit(1)
+		log.Fatalf("Failed to upload %s: %v", args.dst, err)
 	}
 }
